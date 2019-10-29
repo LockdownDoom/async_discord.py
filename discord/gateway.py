ffcdbec90179917708fe69ba5320cdbc6efc5fbe
LockdownDoom <andrewmhett@gmail.com>
2019-10-28 17:26:09 -0700

    Voice connection fixes

--- discord/gateway.py
+++ discord/gateway.py
@@ -546,7 +546,12 @@ class DiscordVoiceWebSocket(websockets.client.WebSocketClientProtocol):
     HEARTBEAT           = 3
     SESSION_DESCRIPTION = 4
     SPEAKING            = 5
+    HEARTBEAT_ACK       = 6
+    RESUME              = 7
     HELLO               = 8
+    INVALIDATE_SESSION  = 9
+    CLIENT_CONNECT      = 12
+    CLIENT_DISCONNECT   = 13
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
